505856cf4895d1619c72c59e7b716769cbdeacdf
SyedAhmadShaan <91550936+SyedAhmadShaan@users.noreply.github.com>
2024-03-26 23:04:57 +0500

    First Commit

---
+++ Index.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to to perform action",
+    type: "list",
+    name: "operator",
+    choices: [
+      "addition",
+      "substraction",
+      "multiplication",
+      "division",
+      "exponentiation",
+      "modulus",
+      "sqaure root",
+      "logarithm",
+    ],
+  },
+]);
+
+console.log(answer);
+
+//conditional statement
+if (answer.operator === "addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "substraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else if (answer.operator === "exponentiation") {
+  console.log(Math.pow(answer.firstNumber, answer.secondNumber));
+} else if (answer.operator === "modulus") {
+  console.log(answer.firstNumber % answer.secondNumber);
+} else if (answer.operator === "sqaure root") {
+  console.log(Math.sqrt(answer.firstNumber));
+} else if (answer.operator === "logarithm") {
+  console.log(Math.log(answer.firstNumber));
+} else {
+  console.log("Please select valid operator");
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# sup3r-simple-calculator
+**Calculator CLI**
+
 This is a simple command-line calculator tool that allows you to perform various mathematical operations.
+
+**Usage**
+
+1. Make sure you have Node.js installed on your machine.
+2. Clone this repository or download the `index.js` file.
+3. Navigate to the directory containing `index.js` in your terminal.
+4. Run the script using Node.js by typing `node index.js`.
+5. Follow the prompts to enter the numbers and select the operation you want to perform.
+
+**Operations Supported**
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Exponentiation
+- Modulus
+- Square Root
+- Logarithm
+
+**Dependencies**
+
+This script uses the following npm package:
+
+- [inquirer](https://www.npmjs.com/package/inquirer) - For interactive command-line prompts.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+**Author Note**
+
+Feel Free to copy / edit/ help others.
+Developed by Syed Ahmad Shaan - PIAIC Student, ID:PIAIC213290
+Email: ahmadthegreat91@gmail.com
+
+`"Calculator"`, `"SimpleCalculator"`, `"Sup3rSimpleCalculator"`
